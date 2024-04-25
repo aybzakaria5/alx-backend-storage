ff63eff919149cadd5f63dee9c5c65016a5c945e
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-04-25 20:37:48 +0000

    task 2 => adding enumeration countries to a tabl

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- create table users if dosen't exist and  enumeration of countries
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
