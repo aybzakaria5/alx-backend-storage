88a41afb2dfddeb9d3e42f932865e1ddc420c233
root <ayoub.don.zakaria@gmail.com>
2024-04-30 16:52:36 -0700

    students score

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""task 102"""
+
+
+def top_students(mongo_collection):
+    students = mongo_collection.find()
+
+    for student in students:
+        total_score = sum(topic['score'] for topic in student['topics'])
+        average_score = total_score / len(student['topics'])
+        student['averageScore'] = average_score
+
+    sorted_students = sorted(students, key=lambda x: x['averageScore'], reverse=True)
+    return sorted_students
