c99fcda6c8341361a6efa55804eccbaab7f95478
aybzakria5 <ayoub.don.zakaria@gmail.com>
2024-05-02 00:21:58 +0000

    adding methodes

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,53 +1,87 @@
 #!/usr/bin/env python3
 """redis"""
+
 import redis
 import uuid
-from typing import Union, Callable
+from typing import Union, Callable, Optional
+from functools import wraps
 
 
 def count_calls(method: Callable) -> Callable:
-        @wraps(method)
-        def wrapper(self, *args, **kwargs):
-            key = method.__qualname__
-            count_key = f"{key}_calls"
-            self._redis.incr(count_key)
-            return method(self, *args, **kwargs)
-        return wrapper
+    """counting calls"""
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """doc for methode"""
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """calling history"""
+    inkey = method.__qualname__ + ":inputs"
+    outkey = method.__qualname__ + ":outputs"
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """doc"""
+        self._redis.rpush(inkey, str(args))
+        res = method(self, *args, **kwargs)
+        self._redis.rpush(outkey, str(res))
+        return res
+
+    return wrapper
+
+
+def replay(method: Callable) -> None:
+    """the replay doc"""
+    input_key = "{}:inputs".format(method.__qualname__)
+    output_key = "{}:outputs".format(method.__qualname__)
+
+    inputs = method.__self__._redis.lrange(input_key, 0, -1)
+    outputs = method.__self__._redis.lrange(output_key, 0, -1)
+
+    print("{} was called {} times:".format(method.__qualname__, len(inputs)))
+    for inp, out in zip(inputs, outputs):
+        print(
+            "{}(*{}) -> {}".format(
+                method.__qualname__, inp.decode("utf-8"), out.decode("utf-8")
+            )
+        )
+
 
 class Cache:
-    """a class for cache"""
+    """cache class docs"""
+
     def __init__(self):
-        """constructure"""
+        """doc doc method"""
         self._redis = redis.Redis()
         self._redis.flushdb()
+
     @count_calls
-
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """dàc for store methode"""
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-        return key
+        """doc doc method"""
+        keyx = str(uuid.uuid4())
+        self._redis.set(keyx, data)
+        return keyx
 
-    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, None]:
-        """docs for get methode"""
-        data = self._redis.get(key)
-        if data is None:
-            return None
-        if fn is not None:
-            return fn(data)
-        return data
+    def get(
+        self, key: str, fn: Optional[Callable] = None
+    ) -> Union[str, bytes, int, float]:
+        """doc doc method"""
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
 
-    def get_str(self, key: str) -> Union[str, None]:
-        return self.get(key, fn=lambda d: d.decode("utf-8"))
+    def get_str(self, key: str) -> str:
+        """doc doc method"""
+        return self.get(key, fn=str)
 
-    def get_int(self, key: str) -> Union[int, None]:
+    def get_int(self, key: str) -> int:
+        """doc doc method"""
         return self.get(key, fn=int)
-
-    def count_calls(method: Callable) -> Callable:
-        @wraps(method)
-        def wrapper(self, *args, **kwargs):
-            key = method.__qualname__
-            count_key = f"{key}_calls"
-            self._redis.incr(count_key)
-            return method(self, *args, **kwargs)
-        return wrapper
